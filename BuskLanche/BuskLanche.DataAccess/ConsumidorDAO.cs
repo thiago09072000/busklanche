51369a755db79175c8b5054e8e16c1ef7cecc1bb
thiago09072000 <thiago09072000@gmail.com>
2018-04-05 20:09:53 -0300

    BuscarTodosConsuDAO
    BuscarTodosConsuDAO

--- BuskLanche/BuskLanche.DataAccess/ConsumidorDAO.cs
+++ BuskLanche/BuskLanche.DataAccess/ConsumidorDAO.cs
@@ -32,5 +32,44 @@ namespace BuskLanche.DataAccess
                 }
             }
         }
+
+        public List<Consumidor> BuscarTodos()
+        {
+            var lstConsumidor = new List<Consumidor>();
+
+            using (SqlConnection conn = new SqlConnection(@"Initial Catalog=BuskLanche; Data Source=localhost; Integrated Security=SSPI;"))
+            {
+                string strSQL = @"SELECT * FROM CadastroConsumidor";
+
+                using (SqlCommand cmd = new SqlCommand(strSQL))
+                {
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = strSQL;
+
+                    var dataReader = cmd.ExecuteReader();
+                    var dt = new DataTable();
+                    dt.Load(dataReader);
+
+                    conn.Close();
+
+                    foreach (DataRow row in dt.Rows)
+                    {
+                        var Consumidor = new Consumidor()
+                        {
+                            Id = Convert.ToInt32(row["idCadConsumidor"]),
+                            Nome = row["nomeConsumidor"].ToString(),
+                            Email = row["emailConsumidor"].ToString(),
+                            Senha = row["senhaConsumidor"].ToString(),
+                            Cpf = row["cpfConsumidor"].ToString()
+
+                        };
+                        lstConsumidor.Add(Consumidor);
+                    }
+                }
+            }
+
+            return (lstConsumidor);
+        }
     }
 }
