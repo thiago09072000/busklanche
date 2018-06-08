af688bdbc5f71ed4ed7a8178a2326678bccf279b
thiago09072000 <thiago09072000@gmail.com>
2018-06-08 20:06:56 -0300

    ajuste
    ajuste

---
+++ BuskLanche/Teste1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using BuskLanche.DataAccess;
+using BuskLanche.Models;
+using BuskLanche.WebUI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Teste1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            ConsumidorDAO L1 = new ConsumidorDAO();
+
+        }
+
+    }
+}
