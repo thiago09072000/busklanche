4611971a083542793e003f0bffe21452621ac556
lukamartinatto <lukmartinatto20@hotmail.com>
2018-06-20 21:39:31 -0300

    Banco de dados
    Banco de dados

--- BuskLanche/BuskLanche.WebUI/banco.sql
+++ BuskLanche/BuskLanche.WebUI/banco.sql
@@ -25,48 +25,7 @@ create table CadastroComercio
 	DescricaoComercio		varchar(max)
 )
 
-insert into CadastroComercio 
-values
-('123456789', 'nomeComercio', 'bairro', 'rua', 134, 'cep', 'complemento', 'nome', 'email', 'senha', 'cpf', 'telefone', 'estiloDoLanche', '22:00', '23:00', 'DescricaoComercio')
-
--- select * from CadastroComercio
-
---update CadastroComercio set nomeComercio = CONCAT(nomeComercio, idCadComercio), cep = '54866-666' where idCadComercio in (1, 2,3);
-
---insert into CadastroComercio (cnpj,
---nomeComercio,
---bairro,
---rua,
---numero,
---cep,
---complemento,
---nomeRepresentante,
---emailRepresentante,
---senhaRepresentante,
---cpfRepresentante,
---telefoneRepresentante,
---estiloDoLanche,
---horarioAbertura,
---horarioEnceramento,
---DescricaoComercio)
---select cnpj,
---nomeComercio,
---bairro,
---rua,
---numero,
---cep,
---complemento,
---nomeRepresentante,
---emailRepresentante,
---senhaRepresentante,
---cpfRepresentante,
---telefoneRepresentante,
---estiloDoLanche,
---horarioAbertura,
---horarioEnceramento,
---DescricaoComercio from CadastroComercio where idCadComercio = 2;
-
-
+select * from CadastroComercio
 
 create table CadastroConsumidor
 (
@@ -87,10 +46,6 @@ create table CadastroDeCardapio
 	preco			decimal(7, 2)
 )
 
-insert into CadastroDeCardapio 
-values
-(1, 'nome', 'ingrediente', 24.00)
-
 --select * from CadastroDeCardapio
 
 create table Avaliacoes
@@ -107,18 +62,3 @@ create table Avaliacoes
 
 select * from Avaliacoes
 
-insert into Avaliacoes (idCadComercio, idCadConsumidor, avaliacao, comentario, dataHora)
-select idCadComercio, idCadConsumidor, avaliacao, comentario, getdate() from Avaliacoes where idCadComercio = 5;
-
-update Avaliacoes set avaliacao = case when idAvaliacoes % 2 = 0 then 3 else 5 end;
-
-
-select cast(avg(cast(avaliacao as decimal(15,2))) as decimal(15,2)) as media_nota from Avaliacoes where idCadComercio = 2;
-select avg(avaliacao) from Avaliacoes where idCadComercio = 5;
-
-
-SELECT 
-                                    *,
-                                    coalesce((select cast(avg(cast(avaliacao as decimal(15,2))) as decimal(15,2)) as media_nota from Avaliacoes where idCadComercio = CadastroComercio.idCadComercio), 0) as media_nota
-                                FROM CadastroComercio 
-								WHERE cep = @cep;
